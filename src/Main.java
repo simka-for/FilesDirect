df7ef7a3e6aac42bf2fd0d4966e31735a4c27408
Simka-for <58855519+simka-for@users.noreply.github.com>
2020-02-02 15:57:14 +0300

    lambdaEdit

--- src/Main.java
+++ src/Main.java
@@ -1,20 +1,20 @@
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
+import java.nio.file.Path;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         File path = new File ("D:/test");   // Устанавливаем свой путь
 
         Long size = Files.walk(path.toPath()).         // Поток всех файлов по введенному пути
-                map(f -> f.toFile())                   // Проходим по всем файлам
-                .filter(f -> f.isFile())               // Проверяет является ли объект файлом
-                .mapToLong(f -> f.length()).sum();     // Считает размер файла и суммирует с остальными
+                map(Path::toFile)                   // Проходим по всем файлам
+                .filter(File::isFile)               // Проверяет является ли объект файлом
+                .mapToLong(File::length).sum();     // Считает размер файла и суммирует с остальными
 
         System.out.println(getFileSizeMb(size));
-
-
     }
+
     private static String getFileSizeMb(Long size){                                     // Вывод в мегабайтах
         return  "Размер директории = " + (double) size/(1024*1024) + "mb";
     }
