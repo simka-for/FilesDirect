722c0033a23f9bb0cafe53375443ff8fc1656414
Simka-for <58855519+simka-for@users.noreply.github.com>
2020-02-02 14:40:11 +0300

    sizeDirectory

--- src/Main.java
+++ src/Main.java
@@ -3,14 +3,22 @@ import java.io.IOException;
 import java.nio.file.Files;
 
 public class Main {
-    public static void main(String[] args) {
-        File path = new File ("D:/spisok");   // Устанавливаем свой путь
-        
+    public static void main(String[] args) throws IOException {
+        File path = new File ("D:/test");   // Устанавливаем свой путь
+
+        Long size = Files.walk(path.toPath()).         // Поток всех файлов по введенному пути
+                map(f -> f.toFile())                   // Проходим по всем файлам
+                .filter(f -> f.isFile())               // Проверяет является ли объект файлом
+                .mapToLong(f -> f.length()).sum();     // Считает размер файла и суммирует с остальными
+
+        System.out.println(getFileSizeMb(size));
 
 
     }
-//    Files.walk(new File("D:/temp").toPath())
-//            .map(f -> f.toFile())
-//            .filter(f -> f.isFile())
-//            .mapToLong(f -> f.length()).sum();
+    private static String getFileSizeMb(Long size){                                     // Вывод в мегабайтах
+        return  "Размер директории = " + (double) size/(1024*1024) + "mb";
+    }
+    private static String getFileSizeGb(Long size) {                                   // Вывод в гигабайтах
+        return "Размер директории = " + (double) size / (1024 * 1024 * 1024) + "gb";
+    }
 }
