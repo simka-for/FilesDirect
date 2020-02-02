e7c076a5a345546aebeedb140d8a4e6c92471cc8
Simka-for <58855519+simka-for@users.noreply.github.com>
2020-02-02 17:01:27 +0300

    add 2 metod

--- src/Main.java
+++ src/Main.java
@@ -5,14 +5,16 @@ import java.nio.file.Path;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        File path = new File ("D:/test");   // Устанавливаем свой путь
-
-        Long size = Files.walk(path.toPath()).         // Поток всех файлов по введенному пути
-                map(Path::toFile)                   // Проходим по всем файлам
-                .filter(File::isFile)               // Проверяет является ли объект файлом
-                .mapToLong(File::length).sum();     // Считает размер файла и суммирует с остальными
+        // Первый метод
+        File path = new File ("D:/AZERBAIJAN");   // Устанавливаем свой путь
+        Long size = Files.walk(path.toPath()).             // Поток всех файлов по введенному пути
+                map(Path::toFile)                          // Проходим по всем файлам
+                .filter(File::isFile)                      // Проверяет является ли объект файлом
+                .mapToLong(File::length).sum();            // Считает размер файла и суммирует с остальными
 
         System.out.println(getFileSizeMb(size));
+
+        System.out.println(getFileSizeMb(folderSize(path)));
     }
 
     private static String getFileSizeMb(Long size){                                     // Вывод в мегабайтах
@@ -20,5 +22,17 @@ public class Main {
     }
     private static String getFileSizeGb(Long size) {                                   // Вывод в гигабайтах
         return "Размер директории = " + (double) size / (1024 * 1024 * 1024) + "gb";
+    }
+
+    // Второй метод
+    public static long folderSize(File directory) {
+        long length = 0;
+        for (File file : directory.listFiles()) {
+            if (file.isFile())
+                length += file.length();
+            else
+                length += folderSize(file);
+        }
+        return length;
     }
 }
