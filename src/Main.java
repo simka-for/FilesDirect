d81111543711bbbfba533e5a80562f5cac1eec9e
Simka-for <58855519+simka-for@users.noreply.github.com>
2020-02-02 13:58:00 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+
+public class Main {
+    public static void main(String[] args) {
+        File path = new File ("D:/spisok");   // Устанавливаем свой путь
+        
+
+
+    }
+//    Files.walk(new File("D:/temp").toPath())
+//            .map(f -> f.toFile())
+//            .filter(f -> f.isFile())
+//            .mapToLong(f -> f.length()).sum();
+}
